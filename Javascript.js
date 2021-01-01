08c0a10add0c23fbd065a72aae59241006b10ca0
AbdelrahmanShamardal <63594642+AbdelrahmanShamardal@users.noreply.github.com>
2021-01-02 01:33:04 +0200

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,13 @@
+// window.addEventListener('keydown' , function(e){
+// console.log (e);
+// })
+
+document.addEventListener("click", myFunction);
+document.addEventListener("click", someOtherFunction);
+
+function myFunction() {
+  alert ("Hello World!");}
+
+
+function someOtherFunction() {
+  alert ("This function was also executed!");}
